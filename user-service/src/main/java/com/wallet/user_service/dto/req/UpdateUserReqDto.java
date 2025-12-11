dcebfb0534170813b6ea9891f6ede0ba4f45f0a4
Nidhi Nayak <nidhinayak31@gmail.com>
2025-12-11 15:42:09 +0530

    create req and res dto for user-service

---
+++ user-service/src/main/java/com/wallet/user_service/dto/req/UpdateUserReqDto.java
@@ -0,0 +1,27 @@
+package com.wallet.user_service.dto.req;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserReqDto {
+	
+	@NotBlank
+    private String fullName;
+
+	
+	@NotEmpty
+	private String mobileNumber;
+
+	@NotBlank
+	private String address;
+}
