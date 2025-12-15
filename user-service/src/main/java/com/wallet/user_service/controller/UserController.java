77d456d661b90b04c9935e9cec92d94803c1e5d0
Nidhi Nayak <nidhinayak31@gmail.com>
2025-12-15 17:35:19 +0530

    User-service controller logic added

--- user-service/src/main/java/com/wallet/user_service/controller/UserController.java
+++ user-service/src/main/java/com/wallet/user_service/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.wallet.user_service.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,25 +17,29 @@ import com.wallet.user_service.dto.res.UserResponse;
 import com.wallet.user_service.service.UserService;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/users")
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserController {
 	
 	private UserService userService;
 	
-	public ResponseEntity<UserResponse> getUserById(){
-		return null;
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserResponse> getUserById(@PathVariable Long userId){
+		return ResponseEntity.ok(userService.getUserById(userId));
 	}
 	
+	@PostMapping("/create")
 	public ResponseEntity<UserResponse> createUser(@Valid @RequestBody CreateUserReqDto dto){
-		return null;
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(dto));
 	}
 	
-	public ResponseEntity<UserResponse> updateUser(@Valid @RequestBody UpdateUserReqDto dto){
-		return null;
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserResponse> updateUser(@PathVariable Long userId,@Valid @RequestBody UpdateUserReqDto dto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.updateUser(userId, dto));
 	}
 }
