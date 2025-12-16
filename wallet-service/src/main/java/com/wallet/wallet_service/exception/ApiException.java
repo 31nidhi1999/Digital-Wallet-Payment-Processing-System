1f4229552d1a36fb1ef1ad0e456d18537d03d348
Nidhi Nayak <nidhinayak31@gmail.com>
2025-12-16 11:54:36 +0530

    added centralized exception handeling

---
+++ wallet-service/src/main/java/com/wallet/wallet_service/exception/ApiException.java
@@ -0,0 +1,9 @@
+package com.wallet.wallet_service.exception;
+
+public class ApiException extends RuntimeException {
+
+	public ApiException(String message) {
+		super(message);
+	}
+	
+}
--- wallet-service/src/main/java/com/wallet/wallet_service/exception/GlobalExceptionHandler.java
+++ wallet-service/src/main/java/com/wallet/wallet_service/exception/GlobalExceptionHandler.java
@@ -1,9 +1,8 @@
-package com.wallet.user_service.exception;
+package com.wallet.wallet_service.exception;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.jspecify.annotations.Nullable;
 import org.springframework.http.HttpStatus;
@@ -14,22 +13,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.wallet.user_service.dto.res.ApiResponse;
+import com.wallet.wallet_service.dto.res.ApiResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-	@ExceptionHandler(UserAlreadyExistsException.class)
-	@ResponseStatus(value = HttpStatus.CONFLICT)
-	public ApiResponse handleUserAlreadyExistsException(RuntimeException e) {
-		return new ApiResponse(e.getMessage());
-	}
-	
-	@ExceptionHandler(ResourceNotFoundException.class)
-	@ResponseStatus(value = HttpStatus.NOT_FOUND)
-	public ApiResponse handleResourceNotFound(RuntimeException e) {
-		return new ApiResponse(e.getMessage());
-	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
@@ -38,6 +25,18 @@ public class GlobalExceptionHandler {
 		Map<String, String> map = fieldErrors.stream()
 				.collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map);
+	}
+	
+	@ExceptionHandler(WalletAlreadyExistsException.class)
+	@ResponseStatus(value = HttpStatus.CONFLICT)
+	public ApiResponse handleWalletExistsException(RuntimeException e) {
+		return new ApiResponse(e.getMessage());
+	}
+	
+	@ExceptionHandler(WalletAlreadyExistsException.class)
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public ApiResponse handleWalletNotFoundException(RuntimeException e) {
+		return new ApiResponse(e.getMessage());
 	}
 	
 	@ExceptionHandler(RuntimeException.class)
